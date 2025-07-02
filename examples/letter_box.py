3a4d46fd02990a4b3880738b075887eaaac496c4
Nathaniel D. Hoffman <dene@cmu.edu>
2025-07-02 00:15:14 -0400

    fix: bandaid patch the letter_box example
    Boxes aren't working right, I'll probably have to go through this code very carefully

--- examples/letter_box.py
+++ examples/letter_box.py
@@ -1,9 +1,9 @@
-from textdraw import BoundingBox, Box, Pixel, PixelGroup, Point, TextPath, render
+from textdraw import BoundingBox, Box, Pixel, PixelGroup, Point, TextPath, multipath, render
 
 
 class LetterBox:
     def __init__(self, letter: str, x: int, y: int):
-        self.box = Box(letter, (x, y))
+        self.box = Box(letter, (x, y), width=5, height=3)
         self.c_right = self.box.bbox.center_right + Point(1, 0)
         self.c_left = self.box.bbox.center_left - Point(1, 0)
         self.c_top = self.box.bbox.top_center + Point(0, 1)
@@ -28,10 +28,10 @@ if __name__ == '__main__':
     b = LetterBox('b', 20, -8)
     c = LetterBox('c', 3, -10)
     bbox = BoundingBox.wrap(a.box, b.box, c.box)
-    bbox.top += 3
-    bbox.bottom -= 3
-    bbox.left -= 3
-    bbox.right += 3
+    bbox.top += 7
+    bbox.bottom -= 7
+    bbox.left -= 7
+    bbox.right += 7
 
     all_barriers = [a.barriers, b.barriers, c.barriers, a.box, b.box, c.box]
     paths = []
@@ -86,42 +86,53 @@ if __name__ == '__main__':
             bbox=bbox,
         )
     )
-    shared_paths = [
-        TextPath(
-            c.c_bottom,
-            a.c_right,
-            style='yellow',
-            line_style='thick',
-            bend_penalty=20,
-            environment=paths,
-            barriers=all_barriers,
-            bbox=bbox,
-        )
-    ]
-    shared_paths.append(
-        TextPath(
-            b.c_left,
-            c.c_right,
-            style='yellow',
-            line_style='thick',
-            bend_penalty=20,
-            paths=shared_paths,
-            environment=paths,
-            barriers=all_barriers,
-            bbox=bbox,
-        )
+    shared_paths = multipath(
+        [c.c_bottom, b.c_left, a.c_top],
+        [a.c_right, c.c_right, b.c_right],
+        style='yellow',
+        line_style='thick',
+        bend_penalty=20,
+        environment=paths,
+        barriers=all_barriers,
+        bbox=bbox,
+        optimize=True,
     )
-    shared_paths.append(
-        TextPath(
-            a.c_top,
-            b.c_right,
-            style='yellow',
-            line_style='thick',
-            bend_penalty=20,
-            paths=shared_paths,
-            environment=paths,
-            barriers=all_barriers,
-            bbox=bbox,
-        )
-    )
+    # shared_paths = [
+    #     TextPath(
+    #         c.c_bottom,
+    #         a.c_right,
+    #         style='yellow',
+    #         line_style='thick',
+    #         bend_penalty=20,
+    #         environment=paths,
+    #         barriers=all_barriers,
+    #         bbox=bbox,
+    #     )
+    # ]
+    # shared_paths.append(
+    #     TextPath(
+    #         b.c_left,
+    #         c.c_right,
+    #         style='yellow',
+    #         line_style='thick',
+    #         bend_penalty=20,
+    #         paths=shared_paths,
+    #         environment=paths,
+    #         barriers=all_barriers,
+    #         bbox=bbox,
+    #     )
+    # )
+    # shared_paths.append(
+    #     TextPath(
+    #         a.c_top,
+    #         b.c_right,
+    #         style='yellow',
+    #         line_style='thick',
+    #         bend_penalty=20,
+    #         paths=shared_paths,
+    #         environment=paths,
+    #         barriers=all_barriers,
+    #         bbox=bbox,
+    #     )
+    # )
     print(render(a.box, b.box, c.box, *paths, *shared_paths))
